e50faa46a634a33a35679a61b01a7c89748933a9
marcoseluz <marcos.e.luz@aluno.senai.br>
2025-10-02 16:49:09 -0300

    Add files via upload

--- Calculos/logica.js
+++ Calculos/logica.js
@@ -1,9 +1,40 @@
-function subtracao (){
+function soma(){
     var numero1 = document.getElementById("n1").valueAsNumber
     var numero2 = document.getElementById("n2").valueAsNumber
 
-    var resposta = document.getElementById("respsubtracao")
+    var resposta = document.getElementById("respSoma")
+    
+    resposta.textContent = numero1 + numero2
+    //console.log(numero!, numero2) 
+
+
+}
+
+function sub(){
+    var numero1 = document.getElementById("n3").valueAsNumber
+    var numero2 = document.getElementById("n4").valueAsNumber
+
+    var resposta = document.getElementById("respSub")
     
     resposta.textContent = numero1 - numero2
-    //console.log(numero!, numero2) 
+    //console.log(numero!, numero2)
+}
+
+function multi(){
+    var number1 = parseFloat(document.getElementById("n5").value)
+    var number2 = parseFloat(document.getElementById("n6").value)
+    document.getElementById("respMulti").textContent = number1 * number2
+}
+
+function div (){
+   var a = parseFloat(document.getElementById("n7").value)
+   var b = parseFloat(document.getElementById("n8").value)
+   var resp = document.getElementById("respDiv")
+
+   if(b !==0 ){
+    resp.innerHTML = (a / b).toFixed(2)
+}else{
+    resp.innerHTML = "nao  se divide por 0"}
+  
+
 }
